c101b89cedb29bce71d841e94364fe724dafd136
HermanoCastro65 <hermanocastro18@gmail.com>
2026-03-06 16:12:10 -0300

    test(scanner): add directory scanner test and register in test runner

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -2,10 +2,12 @@
 
 void basic_test();
 void build_artifact_test();
+void directory_scanner_lists_files();
 
 int main() {
   run_test("basic_test", basic_test);
   run_test("build_artifact_test", build_artifact_test);
+  run_test("directory_scanner_lists_files", directory_scanner_lists_files);
 
   print_summary();
 
