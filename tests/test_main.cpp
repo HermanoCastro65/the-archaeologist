bc4dea77e9771f0044e25165a3998230aed48528
HermanoCastro65 <hermanocastro18@gmail.com>
2026-03-06 21:08:50 -0300

    test(runner): add self-runner project scan test

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -4,12 +4,14 @@ void basic_test();
 void build_artifact_test();
 void directory_scanner_lists_files();
 void directory_scanner_list_outputs_files();
+void self_runner_scans_project();
 
 int main() {
   run_test("basic_test", basic_test);
   run_test("build_artifact_test", build_artifact_test);
   run_test("directory_scanner_lists_files", directory_scanner_lists_files);
   run_test("directory_scanner_list_outputs_files", directory_scanner_list_outputs_files);
+  run_test("self_runner_scans_project", self_runner_scans_project);
 
   print_summary();
 
